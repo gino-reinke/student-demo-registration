f42d2e6c3d9b09d713ae1a83da166faf4528c8bb
Gino Reinke <ginpreinke@gmail.com>
2025-04-20 13:39:10 -0400

    Started choose role component

--- src/components/ChooseRole/ChooseRole.jsx
+++ src/components/ChooseRole/ChooseRole.jsx
@@ -1,11 +1,48 @@
 import React from 'react';
 import styles from './ChooseRole.module.css';
-import { getImageUrl } from '../../utils';
 
-export const LeftPane = () => {
+export const ChooseRole = () => {
   return (
     <div className={styles.contain}>
+      <div className={styles['scroll-view']}>
+        <span className={styles.text}>Choose Your Role</span>
+        <span className={styles.text2}>
+          Select whether you are a student registering or the professor managing the schedule.
+        </span>
 
+        <div className={styles['row-view']}>
+          <img
+            src="https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5nCltVnIbi/wzocncxx_expires_30_days.png"
+            className={styles.image}
+            alt="Student icon"
+          />
+          <div className={styles.column}>
+            <span className={styles.text3}>Student</span>
+            <span className={styles.text4}>
+              Select your preferred time slot, or view/reselect your time slot.
+            </span>
+          </div>
+          <img
+            src="https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5nCltVnIbi/21z8ztzv_expires_30_days.png"
+            className={styles.image2}
+            alt="Student icon 2"
+          />
+        </div>
+
+        <div className={styles['row-view2']}>
+          <img
+            src="https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5nCltVnIbi/2f4aa70o_expires_30_days.png"
+            className={styles.image}
+            alt="Professor icon"
+          />
+          <div className={styles.column2}>
+            <span className={styles.text3}>Professor</span>
+            <span className={styles.text4}>
+              Manage time slots and view all student project registrations.
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
