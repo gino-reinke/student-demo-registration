f1cc91192ef7fceea14b53fc02cdcc9fc194da31
Gino Reinke <ginpreinke@gmail.com>
2025-04-20 14:14:27 -0400

    Added octagons around images, and updated styling on ChooseRole component

--- src/components/ChooseRole/ChooseRole.jsx
+++ src/components/ChooseRole/ChooseRole.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './ChooseRole.module.css';
+import { getImageUrl } from '../../utils';
 
 export const ChooseRole = () => {
   return (
@@ -11,11 +12,12 @@ export const ChooseRole = () => {
         </span>
 
         <div className={styles['row-view']}>
-          <img
-            src="https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5nCltVnIbi/wzocncxx_expires_30_days.png"
-            className={styles.image}
-            alt="Student icon"
-          />
+        <div className={styles.octagon}>
+            <img
+                src={getImageUrl("chooserole/coffee.svg")}
+                alt="Student icon"
+            />
+        </div>
           <div className={styles.column}>
             <span className={styles.text3}>Student</span>
             <span className={styles.text4}>
@@ -23,18 +25,19 @@ export const ChooseRole = () => {
             </span>
           </div>
           <img
-            src="https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5nCltVnIbi/21z8ztzv_expires_30_days.png"
+            src={getImageUrl("chooserole/arrowRight.svg")}
             className={styles.image2}
             alt="Student icon 2"
           />
         </div>
 
         <div className={styles['row-view2']}>
-          <img
-            src="https://storage.googleapis.com/tagjs-prod.appspot.com/v1/5nCltVnIbi/2f4aa70o_expires_30_days.png"
-            className={styles.image}
-            alt="Professor icon"
-          />
+        <div className={styles.octagon}>
+            <img
+                src={getImageUrl("chooserole/briefcase.svg")}
+                alt="Professor icon"
+            />
+        </div>
           <div className={styles.column2}>
             <span className={styles.text3}>Professor</span>
             <span className={styles.text4}>
